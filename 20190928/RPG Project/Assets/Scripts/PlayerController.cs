679b7e773d6733ff49b290d7d82384c94e01ab7f
jujujujujuju96 <zhaozihaotj@gmail.com>
2019-09-28 11:28:30 +0900

    20190928
    E09

---
+++ 20190928/RPG Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public Interactable focus;
+
+    Camera cam;
+    public LayerMask movemenMask;
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+            if (Physics.Raycast(ray,out Hit,100,movemenMask)){
+                Debug.Log("We Hit " + Hit.collider.name + " " + Hit.point);
+                motor.MoveToPoint(Hit.point);
+            }
+
+            RemoveFocus();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+
+            if (Physics.Raycast(ray, out Hit, 100)) {
+                Interactable interactable = Hit.collider.GetComponent<Interactable>();
+                if (interactable != null) {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void SetFocus(Interactable newFocus) {
+        if (newFocus != focus) {
+            if (focus != null) {
+                focus.OnDeFocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus() {
+        if (focus != null) {
+            focus.OnDeFocused(); 
+        }
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
---
+++ 20190928/RPG Project/Assets/Scripts/Stats/CharactorStats.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+
+public class CharactorStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    public Stat armor;
+    public Stat damage;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            takeDamage(10);
+        }
+    }
+
+    public void takeDamage(int damage) {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes damage: " + damage);
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    public virtual void Die() {
+        //死亡
+        Debug.Log(transform.name + " Died.");
+    }
+}
