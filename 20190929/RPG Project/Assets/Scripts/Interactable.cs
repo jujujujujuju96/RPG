e0e6539423a8b7061d8c38b572114a2fe9cde6aa
jujujujujuju96 <zhaozihaotj@gmail.com>
2019-09-29 23:09:01 +0900

    20190929
    E09 Enemy

---
+++ 20190929/RPG Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3.0f;
+
+    bool isFocus = false;
+    Transform player;
+    bool hasInteracted = false;
+
+    public Transform interactionTransform;
+
+    public virtual void Interact() {
+
+        Debug.Log("Interact with: "+ transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocus && !hasInteracted) {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius) {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null) {
+            interactionTransform = transform;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDeFocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+}
---
+++ 20190929/RPG Project/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharactorStats
+{
+    private void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem) {
+
+        if (newItem != null) {
+            armor.AddModifier(newItem.armorModifire);
+            damage.AddModifier(newItem.damageModifire);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifire);
+            damage.RemoveModifier(oldItem.damageModifire);
+        }
+
+    }
+}
